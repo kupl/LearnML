65905b1124086a5c47a1c44ac33a372da7ca5033
JunhoL <wnsgh1906@korea.ac.kr>
2018-01-03 10:19:59 +0900

    minor

--- engine/main.ml
+++ engine/main.ml
@@ -41,6 +41,7 @@ let fix_with_solution : prog -> prog -> examples -> unit
         let _ = Type.run prog in 
         let hole_type = !Type.hole_tbl in
         let variable_type = !Type.at_hole_ttbl in
+        (*let (hole_type,variable_type) = Type.run prog in*)
         (n,prog,bounded_var,hole_type,variable_type)
     ) ranked_prog_set in
   let components = Comp.extract_component solution in
