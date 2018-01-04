982ea15171f9d7fb96d03cefb926a7582788a432
ssss0902 <92tnrud92@gmail.com>
2018-01-04 11:32:35 +0900

    upload benchmarks crazy2add

--- benchmarks/crazy2add/15/compile_err/20150929224538.ml
+++
@@ -1,8 +0,0 @@
-let rec crazy2add (x,y) = 
-	match x,y with
-	| n, NIL | NIL, n -> n
-	| MONE(x'), ONE(y') | ZERO(x'), ZERO(y') | ONE(x'), MONE(y') -> ZERO(crazy2add(x',y'))
-	| MONE(x'), ZERO(y') | ZERO(x'), MONE(y') -> MONE(crazy2add(x',y'))
-	| ONE(x'), ZERO(y') | ZERO(x'), ONE(y') ->  ONE(crazy2add(x',y'))
-	| MONE(x'), MONE(y') -> ZERO(crazy2add(crazy2add(x',y'),MONE(NIL)))
-	| ONE(x'), ONE(y') -> ZERO(crazy2add(crazy2add(x',y'),ONE(NIL)))
