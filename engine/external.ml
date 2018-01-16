31408b13ddd7f1c576e0b5656f15945ad514902a
JunhoL <wnsgh1906@korea.ac.kr>
2018-01-16 12:04:27 +0900

    minor

--- engine/external.ml
+++ engine/external.ml
@@ -1,10 +1,13 @@
 open Lang
 
-let init () =
+let usage_msg = "main.native -run (or -fix) -submission <filename> -solution <filename>"
+
+let init_prog () =
+  let _ = Arg.parse Options.options (fun s->()) usage_msg in
   let (_,prog) = 
     Preproc.preprocess_file (!Options.opt_external_filename)
     |> Lexing.from_string
     |> Parser.prog Lexer.token
   in prog
 
-let init_prog = init ()
+let init_prog = init_prog ()
