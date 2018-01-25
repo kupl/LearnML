b9e14168db94f68c084fe45153fd35c788004516
sdw0316 <sdw0316@gmail.com>
2018-01-25 13:50:34 +0900

    2017_1

--- benchmarks/KoreaUniv/2017_1/sub53.ml
+++
@@ -1,9 +0,0 @@
-let rec fastexpt b n =
-if n = 0 then 1
-else if n mod 2 =0 then ( fastexpt (b*b) (n/2))
-else b*(fastexpt b (n-1))
-
-let smallest_divisor n =
-   let rec sd n i = if i>(n/2) then n
-   else if (n mod i = 0) then i
-   else sd n (i+1) in sd n 2
