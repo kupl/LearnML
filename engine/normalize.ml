78cc486683f2d3a2b59174ad2eced80aa77946af
sdw0316 <sdw0316@gmail.com>
2018-02-07 16:31:30 +0900

    normalize - simplification

--- engine/normalize.ml
+++ engine/normalize.ml
@@ -1,187 +1,168 @@
 open Lang
 open Util
-(*****************Constant propagation***************************)
-let rec constant_exp : exp -> exp
-= fun exp ->
-  match exp with
-  | ADD (e1,e2) ->
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    begin match e1,e2 with
-    |Const n1,Const n2 -> Const(n1+n2)
-    |Const n1,_ -> if(n1=0) then e2 else ADD(e1,e2)
-    |_,Const n2 -> if(n2=0) then e1 else ADD(e1,e2)
-    |_ -> ADD(e1,e2)
-    end 
-  | MUL (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    begin match e1,e2 with
-    |Const n1,Const n2 -> Const(n1*n2)
-    |Const n1,_ -> if(n1=0) then Const 0 else if (n1=1) then e2 else MUL(e1,e2)
-    |_,Const n2 -> if(n2=0) then Const 0 else if (n2=1) then e1 else MUL(e1,e2)
-    |_ -> MUL(e1,e2)
-    end
-  | OR (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e1=FALSE && e2=FALSE) then FALSE else if(e1=TRUE || e2=TRUE) then TRUE else OR(e1,e2) 
-  | AND (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e1=TRUE && e2=TRUE) then TRUE else if (e1=FALSE || e2=FALSE) then FALSE else AND(e1,e2)
-  | EQUAL (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if (e1=e2) then TRUE else EQUAL(e1,e2)
-  | NOTEQ (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if (e1!=e2) then TRUE else NOTEQ(e1,e2)
-  | SUB (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e1=e2) then Const 0 
-    else
-    begin match e1,e2 with
-    |Const n1,Const n2 -> Const(n1-n2)
-    |_,Const n2 -> if(n2=0) then e1 else SUB(e1,e2)
-    |_ -> SUB(e1,e2)
-    end 
-  | DIV (e1,e2) ->
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e2=Const 0) then DIV(e1,e2)
-    else if(e1=e2) then Const 1 
-    else
-    begin match e1,e2 with
-    |Const n1,Const n2 -> Const(n1 / n2)
-    |_,Const n2 -> if(n2=1) then e1 else DIV(e1,e2)
-    |_ -> DIV(e1,e2)
-    end 
-  | LESS (e1,e2) ->
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e1=e2) then FALSE
-    else
-    begin match e1,e2 with
-    |Const n1,Const n2 -> if(n1<n2) then TRUE else FALSE
-    |_ -> LESS(e1,e2)
-    end 
-  | LARGER (e1,e2) ->
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e1=e2) then FALSE
-    else
-    begin match e1,e2 with
-    |Const n1,Const n2 -> if(n1>n2) then TRUE else FALSE
-    |_ -> LARGER(e1,e2)
-    end 
-  | LESSEQ (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e1=e2) then TRUE
-    else
-    begin match e1,e2 with
-    |Const n1,Const n2 -> if(n1<=n2) then TRUE else FALSE
-    |_ -> LESSEQ(e1,e2)
-    end
-  | LARGEREQ (e1,e2) ->
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e1=e2) then TRUE
-    else
-    begin match e1,e2 with
-    |Const n1,Const n2 -> if(n1>=n2) then TRUE else FALSE
-    |_ -> LARGEREQ(e1,e2)
-    end 
-  | EApp (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    EApp (e1,e2)
-  | MOD (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    if(e2=Const 0) then MOD(e1,e2)
-    else if(e1=e2) then Const 0 
-    else
-    begin match e1,e2 with
-    |Const n1,Const n2 -> Const(n1 mod n2)
-    |_,Const n2 -> if(n2=1) then Const 0 else MOD(e1,e2)
-    |_ -> MOD(e1,e2)
-    end
-  | AT (e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    begin match e1,e2 with
-    |EList l1,EList l2 -> EList (l1@l2)
-    |EList l1,_ -> if(l1=[]) then e2 else AT (e1,e2)
-    |_,EList l2 -> if(l2=[]) then e1 else AT (e1,e2)
-    |_ -> AT(e1,e2)
-    end
-  | DOUBLECOLON (e1,e2) ->
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    begin match e2 with
-    |EList l1 -> if(l1=[]) then e1 else DOUBLECOLON(e1,e2)
-    |_ -> DOUBLECOLON(e1,e2)
-    end 
-  | MINUS e -> 
-    let e = constant_exp e in
-    begin match e with
-    |Const 0 -> Const 0
-    |_ -> MINUS e
-    end
-  | NOT e -> 
-    let e = constant_exp e in
-    if(e=TRUE) then FALSE else if (e=FALSE) then TRUE else NOT e
-  | EFun (a,e) -> 
-    let e = constant_exp e in
-    EFun (a,e)
-  | IF (e1,e2,e3) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    let e3 = constant_exp e3 in
-    begin match e1 with
-    |TRUE -> e2
-    |FALSE -> e3
-    |_ -> IF(e1,e2,e3)
-    end
-  | ELet (x,is_rec,args,t,e1,e2) -> 
-    let e1 = constant_exp e1 in
-    let e2 = constant_exp e2 in
-    ELet(x,is_rec,args,t,e1,e2)
-  | ECtor (x,exps) ->
-    ECtor(x,list_map constant_exp exps)
-  | EList (exps) -> 
-    EList (list_map constant_exp exps)
-  | ETuple (exps) -> 
-    ETuple (list_map constant_exp exps)
-  | EMatch (exp,branches) ->
-    let exp = constant_exp exp in
-    let (pat_list,exp_list) = list_split branches in
-    let exp_list = list_map constant_exp exp_list in
-    let branches = list_combine pat_list exp_list in
-    EMatch (exp,branches) 
-  |_ -> exp
 
-let constant_decl : decl -> decl
-= fun decl ->
-	match decl with
-  | DLet (x,is_rec,args,typ,exp) -> 
-    let exp = constant_exp exp in
-    DLet (x,is_rec,args,typ,exp)
-  | _ -> decl
+(* Program Simplification *)
+module Simplification = struct
 
-let constant_pgm : prog -> prog 
-= fun pgm -> list_map constant_decl pgm
+  let rec simplify_exp : exp -> exp
+  = fun exp ->
+    match exp with
+    | EList es -> EList (List.map simplify_exp es)
+    | ETuple es -> ETuple (List.map simplify_exp es)
+    | ECtor (x, es) -> ECtor (x, List.map simplify_exp es)
+    (* aop *)
+    | MINUS e ->
+      let e = simplify_exp e in
+      begin match e with
+      | Const n -> Const (-n)
+      | _ -> MINUS e
+      end
+    | ADD (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | Const n1, Const n2 -> Const (n1 + n2)
+      | Const 0, e | e, Const 0 -> e
+      | _ -> ADD (e1, e2)
+      end
+    | SUB (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | Const n1, Const n2 -> Const (n1 - n2)
+      | Const 0, e -> MINUS e 
+      | e, Const 0 -> e
+      | _ -> SUB (e1, e2)
+      end
+    | MUL (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | Const n1, Const n2 -> Const (n1 * n2)
+      | Const 0, _ | _, Const 0 -> Const 0
+      | Const 1, e | e, Const 1 -> e
+      | _ -> MUL (e1, e2)
+      end
+    | DIV (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | _, Const 0 -> raise (Failure "Division_by_zero.")
+      | Const n1, Const n2 -> Const (n1 / n2)
+      | Const 0, _ -> Const 0
+      | _, Const 1 -> e1
+      | _ -> DIV (e1, e2)
+      end
+    | MOD (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | _, Const 0 -> raise (Failure "Division_by_zero.")
+      | Const n1, Const n2 -> Const (n1 mod n2)
+      | Const 0, _ -> Const 0
+      | _, Const 1 -> Const 0
+      | _ -> MOD (e1, e2)
+      end
+    (* bop *)
+    | NOT e ->
+      let e = simplify_exp e in
+      begin match e with
+      | TRUE -> FALSE
+      | FALSE -> TRUE
+      | _ -> NOT e
+      end
+    | OR (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | TRUE, _ | _, TRUE -> TRUE
+      | FALSE, e | e, FALSE -> e
+      | _ -> OR (e1, e2)
+      end
+    | AND (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | FALSE, _ | _, FALSE -> FALSE
+      | TRUE, e | e, TRUE -> e
+      | _ -> AND (e1, e2)
+      end
+    (* abop *)
+    | LESS (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | Const n1, Const n2 -> if (n1 < n2) then TRUE else FALSE
+      | _, _ -> if (e1 = e2) then FALSE else LESS (e1, e2)
+      end
+    | LESSEQ (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | Const n1, Const n2 -> if (n1 <= n2) then TRUE else FALSE
+      | _, _ -> if (e1 = e2) then TRUE else LESSEQ (e1, e2)
+      end
+    | LARGER (e1, e2) -> simplify_exp (LESS (e2, e1))
+    | LARGEREQ (e1, e2) -> simplify_exp (LESSEQ (e2, e1))
+    (* equality *)
+    | EQUAL (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      if (e1 = e2) then TRUE else EQUAL (e1, e2)
+    | NOTEQ (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      if (e1 = e2) then FALSE else NOTEQ (e1, e2)
+    (* lop *)
+    | AT (e1, e2) -> 
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | EList es1, EList es2 -> EList (es1 @ es2)
+      | EList es, EList [] | EList [], EList es -> EList es
+      | _ -> AT (e1, e2)
+      end
+    | DOUBLECOLON (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match e2 with
+      | EList es -> EList (e1 :: es)
+      | _ -> DOUBLECOLON (e1, e2)
+      end
+    | STRCON (e1, e2) ->
+      let (e1, e2) = (simplify_exp e1, simplify_exp e2) in
+      begin match (e1, e2) with
+      | String s1, String s2 -> String (s1 ^ s2)
+      | _ -> STRCON (e1, e2)
+      end
+    (* else *)
+    | EApp (e1, e2) -> EApp (simplify_exp e1, simplify_exp e2)
+    | EFun (arg, e) -> EFun (arg, simplify_exp e)
+    | ELet (f, is_rec, args, typ , e1, e2) -> ELet (f, is_rec, args, typ, simplify_exp e1, simplify_exp e2)
+    | EBlock (is_rec, bindings, e2) ->
+      let bindings = List.map (fun (f, is_rec, args, typ, e) -> (f, is_rec, args, typ, simplify_exp e)) bindings in
+      EBlock (is_rec, bindings, simplify_exp e2)
+    | EMatch (e, bs) ->
+      let e = simplify_exp e in
+      let (ps, es) = List.split bs in
+      EMatch (e, List.combine ps (List.map simplify_exp es))
+    | IF (e1, e2, e3) ->
+      let e1 = simplify_exp e1 in
+      begin match e1 with
+      | TRUE -> simplify_exp e2
+      | FALSE -> simplify_exp e3
+      | _ -> IF (e1, simplify_exp e2, simplify_exp e3)
+      end
+    | Raise e -> Raise (simplify_exp e)
+    | _ -> exp
+
+  let rec simplify_decl : decl -> decl
+  = fun decl ->
+    match decl with
+    | DLet (f, is_rec, args, typ, e) -> DLet (f, is_rec, args, typ, simplify_exp e)
+    | DBlock (is_rec, bindings) ->
+      let bindings = List.map (fun (f, is_rec, args, typ, e) -> (f, is_rec, args, typ, simplify_exp e)) bindings in
+      DBlock (is_rec, bindings)
+    | _ -> decl
+
+  let simplify_pgm : prog -> prog
+  = fun pgm -> List.map simplify_decl pgm
+
+end
 
 (*****************Program Reordering***************************)
 
 let rec arg_num : exp -> int
 = fun e ->
-	match e with
-	| ADD (e1,e2) 
+  match e with
+  | ADD (e1,e2) 
   | MUL (e1,e2) 
   | OR (e1,e2) 
   | AND (e1,e2) 
@@ -206,14 +187,14 @@ let rec arg_num : exp -> int
   | EList (exps) 
   | ETuple (exps) -> list_fold (fun e r -> (arg_num e) + r) exps 0
   | EMatch (exp,branches) -> 
-  	let (pats,exps) = list_split branches in 
-  	(arg_num exp)+ (list_fold (fun e r -> (arg_num e) + r ) exps 0)
-	|_ -> 1
+    let (pats,exps) = list_split branches in 
+    (arg_num exp)+ (list_fold (fun e r -> (arg_num e) + r ) exps 0)
+  |_ -> 1
 
 let compare_exp : exp -> exp -> bool
 = fun e1 e2 ->
-	match (e1,e2) with
-	| (EVar x1,EVar x2) -> x1 > x2
+  match (e1,e2) with
+  | (EVar x1,EVar x2) -> x1 > x2
   | (Const x1,Const x2) -> x1 > x2
   | (EVar _,_) -> true
   | (_,EVar _) -> false
@@ -223,7 +204,7 @@ let compare_exp : exp -> exp -> bool
 
 let rec reorder_exp : exp -> exp
 = fun exp ->
-	match exp with	
+  match exp with  
   | ADD (e1,e2) -> if compare_exp e1 e2 then ADD (reorder_exp e1,reorder_exp e2) else ADD (reorder_exp e2,reorder_exp e1)
   | MUL (e1,e2) -> if compare_exp e1 e2 then MUL (reorder_exp e1,reorder_exp e2) else MUL (reorder_exp e2,reorder_exp e1)
   | OR (e1,e2) -> if compare_exp e1 e2 then OR (reorder_exp e1,reorder_exp e2) else OR (reorder_exp e2,reorder_exp e1)
@@ -257,7 +238,7 @@ let rec reorder_exp : exp -> exp
 
 let reorder_decl : decl -> decl
 = fun decl ->
-	match decl with
+  match decl with
   | DLet (x,is_rec,args,typ,exp) -> 
     let exp = reorder_exp exp in
     DLet (x,is_rec,args,typ,exp)
@@ -268,15 +249,7 @@ let reorder_pgm : prog -> prog
 
 let normalize : prog -> prog
 = fun pgm ->
-  let constant_func = (fun x -> constant_pgm x) in
-	let normalize_func = (fun x ->reorder_pgm x) in
+  let constant_func = (fun x -> Simplification.simplify_pgm x) in
+  let normalize_func = (fun x ->reorder_pgm x) in
   let pgm = fix constant_func pgm in
-	fix normalize_func pgm
-
-(************Simple infinite checking********)
-
-let infinite_decl : decl -> bool
-= fun decl -> true
-  
-let check_infinite : prog -> bool
-= fun pgm -> List.for_all infinite_decl pgm
+  fix normalize_func pgm
