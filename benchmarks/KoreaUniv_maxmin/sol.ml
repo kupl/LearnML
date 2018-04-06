261119d8db7026d2598182891afeeb28da37a1e2
junhoL <wnsgh1906@korea.ac.kr>
2018-04-05 21:08:06 -0700

    testcases

--- benchmarks/KoreaUniv_maxmin/sol.ml
+++ benchmarks/KoreaUniv_maxmin/sol.ml
@@ -1,14 +1,12 @@
-let max_int = 4611686018427387903
-
 let rec fold f l a =
 	match l with
 	| [] -> a
-	| hd::tl -> f hd (fold f tl a)
+	| hd::tl -> if tl=[] then hd else f hd (fold f tl a)
 
 let rec max : int list -> int
 = fun lst -> 
-	fold (fun x y -> if x > y then x else y) lst (-max_int)
+	fold (fun x y -> if x > y then x else y) lst 0
 
 let rec min : int list -> int
 = fun lst ->
-	fold (fun x y -> if x<y then x else y) lst max_int
+	fold (fun x y -> if x<y then x else y) lst 0
