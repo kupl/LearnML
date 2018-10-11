6714dfffbaf03a884130c1be44f6bd402b798348
sdw0316 <sdw0316@gmail.com>
2018-10-11 10:57:51 +0900

    erase noise

--- benchmarks/KoreaUniv_wellformedness_17/17/sub17.ml
+++ benchmarks/KoreaUniv_wellformedness_17/17/sub17.ml
@@ -1,38 +1,4 @@
 (**********************)
-(*   Problem 1        *)
-(**********************)
-
-type exp = 
-  | CONST of int
-  | VAR of var
-  | ADD of exp * exp
-  | SUB of exp * exp
-  | MUL of exp * exp
-  | DIV of exp * exp
-  | ISZERO of exp
-  | READ
-  | IF of exp * exp * exp
-  | LET of var * exp * exp
-  | LETREC of var * var * exp * exp
-  | PROC of var * exp
-  | CALL of exp * exp
-and var = string
-
-(* test cases *)
-let pgm1 = LET ("x", CONST 1, VAR "x")
-let pgm2 = 
-  LET ("f", PROC ("x", VAR "x"), 
-    IF (CALL (VAR "f", ISZERO (CONST 0)), 
-        CALL (VAR "f", CONST 11), 
-        CALL (VAR "f", CONST 22)))
-let pgm3 = LET ("x", ADD (CONST 1, ISZERO (CONST 0)), CONST 2)
-
-(* You can define datatypes and helper functions as necessary *)
-let rec expand : exp -> exp 
-= fun exp -> exp (*TODO*)
-
-
-(**********************)
 (*   Problem 2        *)
 (**********************)
 
