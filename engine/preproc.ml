0e4fc14346eca232fed4809f2b9261c835e53a53
sdw0316 <sdw0316@gmail.com>
2019-06-21 13:55:01 +0900

    read multiple solution programs

--- engine/preproc.ml
+++ engine/preproc.ml
@@ -1,7 +1,6 @@
 open Lang 
 
 (* Read program from file *)
-
 let preprocess_file : string -> string
 = fun filename -> 
   let ic = open_in filename in
@@ -15,16 +14,46 @@ let preprocess_file : string -> string
   end;
   List.rev !lines |> String.concat "\n"
 
-let parse_file (f:string) : (examples * prog) =
+let parse_file : string -> examples * prog
+= fun f -> 
   preprocess_file f
-    |> Lexing.from_string
-    |> Parser.prog Lexer.token
+  |> Lexing.from_string
+  |> Parser.prog Lexer.token
 
 let read_prog : string -> prog option
 = fun filename ->
   try 
-    if Sys.file_exists filename then Some (snd (parse_file filename)) 
-    else None 
+    if Sys.file_exists filename then Some (snd (parse_file filename)) else None 
   with _ -> raise (Failure ("parsing error: " ^ filename)) 
 
+(* Read set of programs from directory *)
+let dir_contents : string -> string list
+= fun dir ->
+  Sys.readdir dir
+  |> Array.to_list 
+  |> List.map (Filename.concat dir)
 
+let read_pgms : string -> prog list
+= fun problem ->
+  if Sys.file_exists problem then 
+    let dirs = dir_contents problem in
+    let pgms = List.fold_left (fun pgms dir ->
+      if Sys.is_directory dir then
+        let files = dir_contents dir in
+        List.fold_left (fun pgms f -> 
+          match read_prog f with
+          | Some pgm -> pgm::pgms
+          | None -> pgms
+        ) pgms files
+      else pgms
+    ) [] dirs 
+    in
+    pgms
+  else []
+
+let read_testcases : string -> examples
+= fun file_name ->
+  if file_name = "" then [] 
+  else 
+    try fst (parse_file file_name) 
+    with e -> raise (e) 
