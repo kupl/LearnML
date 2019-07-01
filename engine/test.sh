074f9147365458498c848cc7411ac9733bdacf69
seonho <circuitbreak98@gmail.com>
2019-07-01 11:12:00 +0900

    test

---
+++ engine/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Similar with reference solution
+./main.native -submission ../benchmarks/KoreaUniv_wellformedness/15/sub32.ml -solution ../benchmarks/KoreaUniv_wellformedness/sol.ml
+
+#Motivating example 1
+./main.native -submission ../incorrect_fail.ml -solution ../correct_metro.ml
+
