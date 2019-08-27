5b759198e53b14034d1f4fdf2c6118bf29350562
Seonho Lee <circuitbreak98@gmail.com>
2019-08-28 01:50:16 +0900

    position vector init

--- engine/Data_driven/pos_vector.ml
+++ engine/Data_driven/pos_vector.ml
@@ -1,9 +1,18 @@
 open Lang
-open Vector
+open BatVect
+open Node
 
-module P = struct
-  let dummy = 0
+module Fv = Freq_vector
+type t = (int BatVect.t) BatVect.t
 
-end 
-  
-  
+let freq_init_vector = Fv.init_vector
+let vec_size = List.length freq_init_vector
+
+let init : unit -> t
+= fun () ->
+  let each_vec = BatVect.make 1 0 in 
+  let test' = BatVect.make vec_size each_vec in
+  test'
+ 
+
+
