aa8f4412104a782b21af416c8ce5db8016dab1e7
sdw0316 <sdw0316@gmail.com>
2020-02-04 10:52:16 +0900

    add new testcases

--- benchmarks_incorrect/All_lambda/15/sub29.ml
+++ benchmarks_incorrect/All_lambda/15/sub29.ml
@@ -12,14 +12,14 @@ let rec checkSt_in_Metro (var,lambda) =
 
 let rec makeMetro_wo_St (var,lambda) =
 	match lambda with
-	|V nm -> if(nm = var) then V "2011-13343ysh"
+	|V nm -> if(nm = var) then V "X"
 				   else V nm
 	|P (nm,met) -> P(nm,makeMetro_wo_St(var,met))
 	|C (met1,met2) -> C(makeMetro_wo_St(var,met1),makeMetro_wo_St(var,met2))
 
 let rec check m =
 	match m with
-	|V var -> if(var = "2011-13343ysh") then true
+	|V var -> if(var = "X") then true
 					 else false
 	|P (var,lambda) -> 
 		if(checkSt_in_Metro(var,lambda)) then check (makeMetro_wo_St (var,lambda))
