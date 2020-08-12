91702020a3bd5c9f27bdfb982a0e4a79a60e3fe0
sdw0316 <sdw0316@gmail.com>
2020-08-12 14:47:28 +0900

    remove tests directory

--- tests/func.ml
+++
@@ -1,6 +0,0 @@
-let add2: int -> int
-= fun y -> y+2
-
-let add4: int -> int
-= fun x -> add2 (add2 x)
-
--- tests/nested_func3.ml
+++
@@ -1,7 +0,0 @@
-
-let add4: int -> int
-= fun x -> 
-  let add2: int -> int
-  = fun y -> y+2 
-in add2 (add2 x)
-
