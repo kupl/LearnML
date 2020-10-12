ab6f72b5d5b6bc36cad943bcd4d5a91255f3e676
sdw0316 <sdw0316@gmail.com>
2020-10-12 15:11:18 +0900

    test: Modify benchmarks which have errors

---
+++ benchmarks_correct/lambda/14/sub20.ml
@@ -0,0 +1,37 @@
+(*********************************
+ ** PL::HW[02].Problem[01]      **
+ **                             **
+ ** Mod. Init: 2014-09-25 19:16 **
+ ** Mod. Fin.: 2014-09-25 21:24 **
+ **                             **
+ ** Writ. by : CMS              **
+ *********************************)
+
+type lambda = V of var
+           | P of var * lambda
+           | C of lambda * lambda
+and var = string
+
+let rec checkMap area_list target = 
+	match target with 
+	| V var -> 
+		if List.mem var area_list
+		then true
+		else false
+	| P (id, m) -> 
+		if isName id
+		then
+			if List.mem id area_list
+			then checkMap area_list m
+			else checkMap (id::area_list) m
+		else false
+	| C (m1, m2) -> 
+		if checkMap area_list m1
+		then checkMap area_list m2
+		else false
+and isName str = 
+	match str with
+	| str1 -> true
+
+let check l = checkMap [] l
+
---
+++ benchmarks_incorrect/lambda/k_15/sub39.ml
@@ -0,0 +1,30 @@
+  type lambda = V of var
+           | P of var * lambda
+           | C of lambda * lambda
+  and var = string
+  
+  let rec ptree : var * lambda -> bool
+  =fun (e1,e2) -> match e2 with
+  | P (x,y) -> 
+  (match y with
+	| P (a,b) -> ptree(a,b) || ptree(x,b) || ptree(e1,b)
+	| C (a,b) -> (ptree(x,a)||ptree(x,b)) && (ptree(e1,a)||ptree(e1,b))
+	| V a -> ptree(x,y) || ptree(e1,y)
+	)
+  | C (x,y) -> ptree(e1,x) && ptree(e1,y)
+  | V x -> e1=x
+  
+  let rec ctree : lambda * lambda -> bool
+  =fun (e1,e2) -> match e1,e2 with
+  | P (x1,y1), P (x2,y2) -> ptree(x1,y1) && ptree(x2,y2)
+  | C (x1,y1), P (x2,y2) -> ctree(x1,y1) && ptree(x2,y2)
+  | P (x1,y1), C (x2,y2) -> ptree(x1,y1) && ctree(x2,y2)
+  | C (x1,y1), C (x2,y2) -> ctree(x1,y1) && ctree(x2,y2)
+  | V x, _ -> false
+  | _, V y -> false
+  
+  let rec check : lambda -> bool
+  =fun e -> match e with
+  | P (x,y) -> ptree (x,y)
+  | C (x,y) -> ctree (x,y)
+  | V x -> true
