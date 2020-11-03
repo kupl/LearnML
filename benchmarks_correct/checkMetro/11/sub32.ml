c9ad4e2fc3bc8d5e0661331c9ecd898f33a5acac
sdw0316 <sdw0316@gmail.com>
2020-11-03 12:34:32 +0900

    remove

--- benchmarks_correct/checkMetro/11/sub32.ml
+++
@@ -1,9 +0,0 @@
-type lambda=V of var|P of var * lambda|C of lambda * lambda
-and var=string
-let rec check(m,id_list)=(match m with
-                |V(id)->(List.mem id id_list)
-                |P(id,m1)->if (List.mem id id_list) then check(m1,id_list)
-                else check(m1,id::id_list)
-                |C(m1,m2)->check(m1,id_list)&check(m2,id_list)
-                )
-let rec check mtr=check(mtr,[])
--- benchmarks_incorrect/KoreaUniv_wellformedness/15/sub20.ml
+++
@@ -1,8 +0,0 @@
-type var = string
- type lambda = Var of var
- | P of var * lambda
- | C of lambda * lambda
-
-let check : lambda -> bool
-=fun e -> true
-
